9ba94f71bb86aff46f78f11413671d314719064e
AvidDabbler <walter.k.jenkins@gmail.com>
2020-01-10 23:24:54 -0600

    vid 7

--- movie-library/src/App.js
+++ movie-library/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const welcome = "Hello Walter. You got this!!!";
+
+class App extends Component {
+    toggle = () => {
+
+
+    }
+  render(){return (
     <div className="App">
-      <header className="App-header">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <Welcome text="YES YES YES Y'ALL"/>
+        <a className="App-link" 
+            href="https://reactjs.org" 
+            target="_blank" 
+            rel="noopener noreferrer" >
+                Learn React
         </a>
-      </header>
+        </header>
+        <input type="text" />
+        <button onClick = {this.toggle}>Show Value</button>
     </div>
-  );
+    );
+    }
+}
+
+
+class Welcome extends Component {
+    render(){
+        const {text} = this.props;
+        return (
+            <h1 className="App-title">{text}</h1>
+        )
+    }
 }
 
 export default App;
